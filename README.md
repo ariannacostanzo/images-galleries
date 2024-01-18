8dd7cfc8d7a92d57f353e1763fc1cfcc7b9d02dc
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-18 14:10:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Una galleria di immagini per conoscermi! I miei giochi preferiti e le mie serie tv preferite!
---
+++ js/data.js
@@ -0,0 +1,63 @@
+const videogames = [
+    {
+        name: "Terraria",
+        image: "terraria.jpg"
+    },
+    {
+        name: "Skyrim",
+        image: "skyrim.jpg"
+    },
+    {
+        name: "Elden Ring",
+        image: "elden-ring.jpg"
+    },
+    {
+        name: "Subnautica",
+        image: "subnautica.jpg"
+    },
+    {
+        name: "Dark Souls",
+        image: "dark-souls.avif"
+    },
+    {
+        name: "Assassin's Creed",
+        image: "assassins-creed.jpg"
+    },
+    {
+        name: "Ori and the will of the wisps",
+        image: "ori.jpg"
+    },
+    {
+        name: "Hollow Knight",
+        image: "hollow-knight.jpg"
+    },
+    {
+        name: "God of War",
+        image: "gow.jpg"
+    },
+    {
+        name: "Stardew Valley",
+        image: "stardew-valley.webp"
+    },
+    {
+        name: "Outward",
+        image: "outward.jpg"
+    },
+    {
+        name: "Monster Hunter World",
+        image: "monster-hunter.jpg"
+    },
+    {
+        name: "The Witcher 3",
+        image: "the-witcher.jpg"
+    },
+    {
+        name: "Monster Hunter",
+        image: "monster-hunter.jpg"
+    },
+    {
+        name: "Monster Hunter",
+        image: "monster-hunter.jpg"
+    },
+    
+]
